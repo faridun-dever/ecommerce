f7cbbd23be7f20d196997ef6a4926dd2dd047887
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2023-08-03 10:28:24 +0300

    Static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-*&v1#&_!e!8fmghas-nssha_9r!8dfkxwh@fr$#bh6zqem03t4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    ".vercel.app"
+]
 
 
 # Application definition
@@ -115,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
